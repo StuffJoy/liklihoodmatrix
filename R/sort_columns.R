4fe70bd4f796671d340759b38284ccf2d0dbe706
Otis Johnson <otis.johnson@upf.edu>
2024-03-15 17:35:51 +0100

    explicitly specifying the packages of excelsheets(readxl) and writexlsx(writexl)

--- R/sort_columns.R
+++ R/sort_columns.R
@@ -18,14 +18,14 @@
 sort_columns <- function(file_path, output_file) {
 
   # Get sheet names
-  sheet_names <- excel_sheets(file_path)
+  sheet_names <- readxl::excel_sheets(file_path)
 
   # Create an empty list to store data frames for each sheet
   data_list <- list()
 
   for (sheet_name in sheet_names) {
     # Read data from the current sheet
-    data <- read_xlsx(file_path, sheet = sheet_name)
+    data <- readxl::read_xlsx(file_path, sheet = sheet_name)
 
     # Identify the order of columns except the first column
     column_order <- c(1, order(names(data)[-1]) + 1)
@@ -38,7 +38,7 @@ sort_columns <- function(file_path, output_file) {
   }
 
   # Write data frames to the Excel file
-  write_xlsx(data_list, output_file)
+  writexl::write_xlsx(data_list, output_file)
 
   cat("Columns (except the first column) have been organized alphabetically for all sheets and saved to", output_file, "\n")
 }
